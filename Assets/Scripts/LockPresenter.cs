c7a848792bc6d49ac61bdfe836c62f645a584a92
R1nge <r1ngegamedev@gmail.com>
2025-04-27 19:26:26 +0300

    Progress indicator fix

--- Assets/Scripts/LockPresenter.cs
+++ Assets/Scripts/LockPresenter.cs
@@ -32,52 +32,53 @@ public class LockPresenter : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _startPosition = Input.mousePosition;
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            var mousePosition = Input.mousePosition;
-            var delta = mousePosition - (Vector3)_startPosition;
-            lockModel.SetCurrentAngle((int)-delta.x);
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            _startPosition = Vector2.zero;
-        }
-
-        if (Math.Abs(lockModel.CurrentAngle - lockModel.TargetAngle) < angleThreshold)
-        {
-            lockView.SetHighlight(true);
-
-            _progressTimerCurrent -= Time.deltaTime;
-            if (_progressTimerCurrent <= 0)
-            {
-                lockView.AddProgress();
-                PickNewProgressTimer();
-            }
-
-            _unlockCurrentTimer -= Time.deltaTime;
-            if (_unlockCurrentTimer <= 0)
-            {
-                lockModel.IsLocked = false;
-                Debug.Log("Unlocked");
-            }
-        }
-        else
-        {
-            lockView.SetHighlight(false);
-            _unlockCurrentTimer = UnlockStartTimer;
-            lockView.ResetProgress();
-            PickNewProgressTimer();
-            Debug.Log("Reset timer");
-        }
-
         if (lockModel.IsLocked)
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _startPosition = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                var mousePosition = Input.mousePosition;
+                var delta = mousePosition - (Vector3)_startPosition;
+                lockModel.SetCurrentAngle((int)-delta.x);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _startPosition = Vector2.zero;
+            }
+
+            if (Math.Abs(lockModel.CurrentAngle - lockModel.TargetAngle) < angleThreshold)
+            {
+                lockView.SetHighlight(true);
+
+                _progressTimerCurrent -= Time.deltaTime;
+                if (_progressTimerCurrent <= 0)
+                {
+                    lockView.AddProgress();
+                    PickNewProgressTimer();
+                }
+
+                _unlockCurrentTimer -= Time.deltaTime;
+                if (_unlockCurrentTimer <= 0)
+                {
+                    lockView.AddProgress();
+                    lockModel.IsLocked = false;
+                    Debug.Log("Unlocked");
+                }
+            }
+            else
+            {
+                lockView.SetHighlight(false);
+                _unlockCurrentTimer = UnlockStartTimer;
+                lockView.ResetProgress();
+                PickNewProgressTimer();
+                Debug.Log("Reset timer");
+            }
+
             _timeBeforeNewTargetCurrentTimer -= Time.deltaTime;
             if (_timeBeforeNewTargetCurrentTimer <= 0)
             {
